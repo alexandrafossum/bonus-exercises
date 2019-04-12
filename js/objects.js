d1d3e73c54f759183b916153ee332c06c7ae7402
Edwin Buendia <edwinbuendia@Edwins-MacBook-Pro.local>
2019-04-12 15:14:08 -0500

    objects js TODOs

---
+++ js/objects.js
@@ -0,0 +1,17 @@
+/**
+ * Bonus:
+ * - Create a function named `createBook` that accepts a title and author
+ *   name and returns a book object with the properties described
+ *   previously. Refactor your code that creates the books array to instead
+ *   use your function.
+ * - Create a function named `showBookInfo` that accepts a book object and
+ *   outputs the information described above. Refactor your loop to use your
+ *   `showBookInfo` function.
+ */
+
+function createBook(title, author) {
+    return book = {
+
+    };
+}
+
