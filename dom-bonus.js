a7cc57da0d4d477a2947b340f1d3967920478750
Alexandra <fossum.alexandra@gmail.com>
2019-04-15 16:24:39 -0500

    started bonus

---
+++ dom-bonus.js
@@ -0,0 +1,20 @@
+"use strict";
+
+//TODO: Allow the user to enter a certain number of items and then display them on
+// the page. For each item, prompt the user for the name of the item, and store in
+// an array of items. For each item in the array of items create a new li element
+// with the correct text and append to the DOM for a simpler alternative, build an
+// entire HTML string and set the innerHTML of an existing <ul> element
+
+// var finalList = document.getElementsByTagName('li');
+//
+// var groceryArray = prompt("Enter your grocery list here: ").split(", ");
+//
+// finalList.innerText = groceryArray;
+
+function printGroceryList() {
+    var items = document.getElementById('');
+}
+
+
+
