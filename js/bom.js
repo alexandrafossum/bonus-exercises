5463991cb80e34cce01a9efe63bb9a4e70cb99ee
Edwin Buendia <edwinbuendia@Edwins-MacBook-Pro.local>
2019-04-18 15:29:48 -0500

    added bonus 1 variable

--- js/bom.js
+++ js/bom.js
@@ -1,10 +1,13 @@
 // ================================= BOM BONUSES
-//
-//
+
 // BONUS 1: Build a Jack-In-The-Box
-//
 // Define an object called jackBox.
-//
+
+var jackInTheBox = {
+    triggered: "true",
+    intervalId: null,
+};
+
 //     Include properties for...
 // triggered - whether or not the box has been sprung (should be false by default)
 // intervalId - set to null
@@ -12,7 +15,7 @@
 // after the lyrics, stop the interval, set the triggered property to true, and alert POP!
 //     windUp() - once called will stop the play() method and set triggered to false
 // lyrics - an array with the following elements:
-//
+
 //     "All a-...",
 //         "-round the ...",
 //         "mulberry...",
@@ -29,21 +32,22 @@
 //         "goes the...",
 //         "wea-...",
 //         "-sel."
-//
+
 // Include methods for...
-//
-//
+
+
+
 // BONUS 2: Build a Stop Watch
-//
+
 // Define an object called stopWatch.
-//
+
 //     Include the following properties...
-//
+
 // intervalId
 // count
-//
+
 // Include the following methods...
-//
+
 // start() - starts console logging an increasing count every second
 // pause() - pauses counter
 // reset() - stops counter and resets count to zero
